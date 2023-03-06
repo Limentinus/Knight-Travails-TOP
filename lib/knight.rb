7ff902011e311e20c67725b448d28ea11c1d69eb
LughsClues <cuberfreak@gmail.com>
2023-03-06 14:26:23 +0100

    write search method

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,17 +1,70 @@
-# require board.rb
+require_relative('board.rb')
 
 class Knight
+  attr_accessor :graph, :graph_hash
+
   def initialize(start)
     @start = start
+    @board = Board.new.board_squares
+    @graph = move_graph
+    @graph_hash = nodes_to_hash
   end
 
-  def move_tree(start = @start)
+  def move_graph
+    hash = []
+    @board.each do |square|
+      hash << MoveNode.new(square)
+    end
+    hash
+  end
+
+  def nodes_to_hash
+    graph = {}
+    @graph.each do |node|
+      graph[node.square] = node.jump_squares
+    end
+    graph
+  end
+
+  def search(start, target, graph = @graph_hash)
+    queue = [start]
+    visited = {start => true}
+    parent = {}
+    
+    while !queue.empty?
+      current = queue.shift
+
+      if current == target
+        path = [current]
+        while parent[current]
+          path.unshift(parent[current])
+          current = parent[current]
+        end
+        return path
+      end
+
+      graph[current].each do |neighbor|
+        if !visited[neighbor]
+          visited[neighbor] = true
+          parent[neighbor] = current
+          queue << neighbor
+        end
+      end
+
+    end
+    return nil
 
   end
+
+  def find_node(square)
+    @graph.select { |node| node.square == square }[0]
+  end
+
 end
 
 class MoveNode
   attr_accessor :square, :jump_squares
+
   def initialize(square)
     @square = square
     @jump_squares = possible_moves
@@ -57,5 +110,6 @@ class MoveNode
   end
 end
 
-test_node = MoveNode.new([6,2])
-p test_node.jump_squares
+
+test_knight = Knight.new([0, 0])
+p test_knight.search([0,1], [6,3])
